12256e0f4b0b6a6eedff535bca7964d4375e3676
headhunterjr <anchiforovnick@gmail.com>
2024-11-05 22:24:45 +0200

    connected database, dbcontext and entity classes created

---
+++ WordSnapConsoleApp/WordSnapWPFApp/DAL/Models/Progress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordSnapWPFApp.DAL.Models;
+
+public partial class Progress
+{
+    public int UserRef { get; set; }
+    public int CardsetRef { get; set; }
+    public DateTime? LastAccessed { get; set; }
+    public double? SuccessRate { get; set; }
+    public virtual Cardset CardsetRefNavigation { get; set; } = null!;
+    public virtual User UserRefNavigation { get; set; } = null!;
+}
