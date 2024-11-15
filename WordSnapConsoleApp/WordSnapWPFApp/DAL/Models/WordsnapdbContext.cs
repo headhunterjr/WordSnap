ba676892d193dfe76aa34ae090b6b3c199c24232
headhunterjr <anchiforovnick@gmail.com>
2024-11-15 08:43:39 +0200

    fixed appsetting.json file path

--- WordSnapConsoleApp/WordSnapWPFApp/DAL/Models/WordsnapdbContext.cs
+++ WordSnapConsoleApp/WordSnapWPFApp/DAL/Models/WordsnapdbContext.cs
@@ -28,7 +28,7 @@ public partial class WordsnapdbContext : DbContext
         {
             var configuration = new ConfigurationBuilder()
                     .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                    .AddJsonFile("C:\\Users\\111\\source\\repos\\Sem5 tre\\Sem5 tre\\appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
 
                     .Build();
             var connectionString = configuration.GetConnectionString("WordSnapDatabaseConnection");
